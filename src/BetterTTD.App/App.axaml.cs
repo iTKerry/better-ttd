abc156bb1b085fa119ac85e011c9af34d96f7aa6
Alex K <a.katrynets@gmail.com>
2020-12-24 19:13:55 +0200

    wip

---
+++ src/BetterTTD.App/App.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace BetterTTD.App
+{
+    public class App : Application
+    {
+        public override void Initialize() => AvaloniaXamlLoader.Load(this);
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            new MainWindow().Show();
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ src/BetterTTD.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Logging.Serilog;
+using Avalonia.ReactiveUI;
+
+namespace BetterTTD.App
+{
+    internal static class Program
+    {
+        public static void Main(string[] args) => 
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+
+        private static AppBuilder BuildAvaloniaApp() => 
+            AppBuilder
+                .Configure<App>()
+                .UseReactiveUI()
+                .UsePlatformDetect()
+                .LogToDebug();
+    }
+}
