2e184282934e82136374c95d0e9336fb45e74914
Alex K <a.katrynets@gmail.com>
2020-12-29 14:02:03 +0200

    add other reads

--- src/BetterTTD.FOAN/Network.fs
+++ src/BetterTTD.FOAN/Network.fs
@@ -1,6 +1,7 @@
 ﻿namespace BetterTTD.FOAN
 
 open System
+open System.Text
 
 module Network =
     
@@ -12,7 +13,7 @@ module Network =
     
     let private defaultSize = 2us
     let private defaultPos = 0
-    let private defaultArray = Array.zeroCreate<byte> 1460
+    let private defaultBuf = Array.zeroCreate<byte> 1460
     
     
     let readU16 packet =
@@ -37,11 +38,29 @@ module Network =
 
     let readByte packet =
         let { Size = _; Position = position; Buffer = buffer } = packet
-        ( BitConverter.ToBoolean (buffer, position),
-          { packet with Position = position + 1 } )
+        ( buffer.[position], { packet with Position = position + 1 } )
+    
+    let readBool packet =
+        let (byte, pac) = readByte packet
+        (byte <> 0uy, pac )
+    
+    let readString packet =
+        let { Size = _; Position = position; Buffer = buffer } = packet
+        
+        let rec read (bytes : byte array, pos, buf : byte array) =
+            if pos < buf.Length && buf.[pos] <> 0uy then
+                let byte = [| buf.[pos] |]
+                read (Array.concat [bytes; byte], pos + 1, buf)
+            else
+                (bytes, pos, buf)
+            
+        let (bytes, pos, _) = read (Array.zeroCreate<byte> 0, position, buffer)
+        (Encoding.Default.GetString bytes, { packet with Position = pos })
     
     let createPacket =
-        { Size = defaultSize; Position = defaultPos; Buffer = defaultArray }
+        { Size = defaultSize
+          Position = defaultPos
+          Buffer = defaultBuf }
 
     let createPacketWithBuf buf =
         let (size, pac) = readU16 {createPacket with Buffer = buf }
