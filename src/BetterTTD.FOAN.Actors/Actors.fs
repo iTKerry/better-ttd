36fef2d4aadc770174f20457bf10f02e9018e6f1
Alex K <a.katrynets@gmail.com>
2020-12-30 12:31:47 +0200

    add actors

---
+++ src/BetterTTD.FOAN.Actors/Actors.fs
@@ -0,0 +1,30 @@
+﻿namespace BetterTTD.FOAN.Actors
+
+module Actors =
+
+    open Akka.FSharp
+    
+    let adminCoordinator (mailbox : Actor<_>) =
+        let rec loop() = actor {
+            let! message = mailbox.Receive ()
+            // handle here
+            return! loop()
+        }
+        loop ()
+    
+    let sender (mailbox : Actor<_>) =
+        let rec loop() = actor {
+            let! message = mailbox.Receive ()
+            // handle here
+            return! loop()
+        }
+        loop ()
+    
+    let receiver (mailbox : Actor<_>) =
+        let rec loop() = actor {
+            let! message = mailbox.Receive ()
+            // handle here
+            return! loop()
+        }
+        loop ()
+            
