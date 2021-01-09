ab98a06906e6399278f2b9dceb9b4a39c395af8f
Alex K <a.katrynets@gmail.com>
2021-01-09 12:49:33 +0200

    update actors

--- src/BetterTTD.FOAN.Actors/Actors.fs
+++ src/BetterTTD.FOAN.Actors/Actors.fs
@@ -12,31 +12,41 @@ open BetterTTD.FOAN.Network.PacketModule
 
 module ActorsModule =
 
+    let createSocket (host : string) (port : int) = 
+        let soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+        soc.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true)
+        soc.Connect(host, port)
+        soc
+        
     let sender (socket : Socket) (mailbox : Actor<_>) =
-        let rec loop() = actor {
-            match! mailbox.Receive () with
-            | Packet packet ->
-                let { Buffer = buf; Size = size; } = prepareToSend packet
-                socket.Send (buf, int size, SocketFlags.None) |> ignore
-                ()
-            return! loop()
-        }
+        let rec loop() =
+            actor {
+                match! mailbox.Receive () with
+                | Packet packet ->
+                    let { Buffer = buf; Size = size; } = prepareToSend packet
+                    socket.Send (buf, int size, SocketFlags.None) |> ignore
+                    ()
+                return! loop()
+            }
         loop ()
     
     let receiver (socket : Socket) (mailbox : Actor<_>) =
-        let rec loop () = actor {
-            match! mailbox.Receive () with
-            | ReceiveMsg ->
-                if socket.Connected then
-                    let pac = createPacket
-                    socket.Receive pac.Buffer |> ignore
-                    let msg = packetToMsg pac
-                    printfn "%A" msg
-                else
-                    ()
-            
-            return! loop ()
-        }
+        let rec loop () =
+            actor {
+                let matchPacket = function
+                | AdminServerProtocol protocol -> mailbox.Context.Parent <! Connecting (Protocol protocol)
+                | AdminServerWelcome welcome -> mailbox.Context.Parent <! Connecting (Welcome welcome)
+                
+                match! mailbox.Receive () with
+                | ReceiveMsg ->
+                    if socket.Connected then
+                        let pac = createPacket
+                        socket.Receive pac.Buffer |> ignore
+                        packetToMsg pac |> matchPacket
+                    else
+                        ()
+                return! loop ()
+            }
         loop ()
     
     let adminCoordinator (mailbox : Actor<_>) =
@@ -45,49 +55,45 @@ module ActorsModule =
             actor {
                 return! connected receiver sender socket
             }
-        
-        let rec connecting (receiver : IActorRef) (sender : IActorRef) (socket : Socket) =
+            
+        and connecting (receiver : IActorRef) (sender : IActorRef) (socket : Socket) =
             actor {
-                let matchNest = function
-                    | Protocol protocol ->
-                        printfn "protocol received %A" protocol
-                        connecting receiver sender socket
-                    | Welcome welcome ->
-                        printfn "welcome received"
-                        connecting receiver sender socket
+                let matchConnecting = function
+                | Protocol protocol ->
+                    printfn "protocol received %A" protocol
+                    connecting receiver sender socket
+                | Welcome welcome ->
+                    printfn "welcome received %A" welcome
+                    connecting receiver sender socket
                         
                 match! mailbox.Receive () with
-                | Connecting msg -> return! matchNest msg
+                | Connecting msg -> return! matchConnecting msg
                 | _ -> failwithf "Invalid state operation occured"
                 
                 return! connecting receiver sender socket
             }
-        
-        let rec idle () =
+            
+        and idle () =
             actor {
-                let matchNest = function
-                    | Connect(host, pass, port) ->
-                        let soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
-                        soc.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true)
-                        soc.Connect(host, port)
-                        
-                        let senderRef = spawn mailbox "sender" (sender soc)
-                        let receiverRef = spawn mailbox "receiver" (receiver soc)
-                        
-                        let msg = AdminJoin { Password = pass; AdminName = "BetterTTD"; AdminVersion = "1.0" }
-                        let packet = msgToPacket msg
-                        
-                        mailbox.Context.System.Scheduler.ScheduleTellRepeatedly(
-                            TimeSpan.FromMilliseconds (0.),
-                            TimeSpan.FromMilliseconds (1.),
-                            receiverRef,
-                            ReceiveMsg)
-                        
-                        senderRef <! Packet packet
-                        connecting receiverRef senderRef soc
+                let matchIdle = function
+                | Connect(host, pass, port) ->
+                    let soc = createSocket host port
+                    let senderRef = spawn mailbox "sender" (sender soc)
+                    let receiverRef = spawn mailbox "receiver" (receiver soc)
+                    let msg = AdminJoin { Password = pass; AdminName = "BetterTTD"; AdminVersion = "1.0" }
+                    let packet = msgToPacket msg
+                    
+                    mailbox.Context.System.Scheduler.ScheduleTellRepeatedly(
+                        TimeSpan.FromMilliseconds (0.),
+                        TimeSpan.FromMilliseconds (1.),
+                        receiverRef,
+                        ReceiveMsg)
+                    
+                    senderRef <! Packet packet
+                    connecting receiverRef senderRef soc
                     
                 match! mailbox.Receive () with
-                | Idle msg -> return! matchNest msg
+                | Idle msg -> return! matchIdle msg
                 | _ -> failwithf "Invalid state operation occured"
                     
                 return! idle ()
