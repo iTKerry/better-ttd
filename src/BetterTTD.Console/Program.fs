f117c6dc5f1aa042c0d5d2fa700bd51f2fc22b08
iTKerry <a.katrynets@gmail.com>
2021-02-10 16:37:55 +0200

    wip

---
+++ src/BetterTTD.Console/Program.fs
@@ -0,0 +1,29 @@
+﻿namespace Temp
+
+open System.IO
+
+module Program =
+    
+    open Akka.FSharp
+    
+    let receiver (stream : Stream) (mailbox : Actor<_>) =
+        let rec loop() =
+            actor {
+                let! message = mailbox.Receive ()
+                return! loop()
+            }
+            
+        loop ()
+    
+    let sender (stream : Stream) (mailbox : Actor<_>) =
+        let rec loop() =
+            actor {
+                let! message = mailbox.Receive ()
+                return! loop()
+            }
+            
+        loop ()
+            
+    [<EntryPoint>]
+    let main argv =
+        0
